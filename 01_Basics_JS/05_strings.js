14a122675baa131cf4c2de3ad13a56b598095f77
Himsin1156 <himanshu1156@gmail.com>
2025-08-04 21:25:45 +0200

    Information added about the strings

---
+++ 01_Basics_JS/05_strings.js
@@ -0,0 +1,20 @@
+const empName ="Himanshu Singh"
+let empPhoneNumber = 9667815803;
+let empAge = 30;
+const empEmail ="himanshu1156@gmail.com"
+
+
+// Here is a new and advances way to write a console command to get the output we have used ``and ${} to give our values from variables
+
+console.log(`Hello my name is ${empName}, my mobile number is ${empPhoneNumber}, my age is ${empAge} and my email id is ${empEmail}. Hope you'd like my information`);
+
+
+// Another and Advanced way to initialize the string
+
+const localName = new String('Himanshu');
+
+console.log(localName.length);
+
+
+
+
